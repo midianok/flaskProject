ddc529777f41273b91750c7925c8796dd9771ab0
Илья Гусев <i.gusev@bidzaar.com>
2023-04-03 23:47:02 +0300

    new models

--- app.py
+++ app.py
@@ -31,28 +31,15 @@ def treech_itan(count=5):
     return strings
 
 
-@app.route('/vasya')
-@app.route('/vasya/<int:count>')
-def vasya(count=5):
-    strings = []
-    for i in range(1, count + 1):
-        strings.append({"id": i, "message": vasya_model.make_sentence(tries=50)})
-    return strings
-
-
-with open("treech_general.txt", encoding="utf8") as f:
+with open("treech_general_v2.txt", encoding="utf8") as f:
     text = f.read()
-treech_general_model = markovify.NewlineText(text)
+treech_general_model = markovify.Text(text, well_formed=False)
 
 
-with open("treech_itan.txt", encoding="utf8") as f:
+with open("treech_itan_v2.txt", encoding="utf8") as f:
     text = f.read()
-treech_itan_model = markovify.NewlineText(text)
+treech_itan_model = markovify.Text(text, well_formed=False)
 
-
-with open("vasya.txt", encoding="utf8") as f:
-    text = f.read()
-vasya_model = markovify.NewlineText(text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
