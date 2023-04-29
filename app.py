811f8e335ae35c9949dadcf363673c53799e50d1
Ilya Gusev <i.gusev@bidzaar.com>
2023-04-30 01:58:27 +0300

    agro

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+import openai
 import markovify
 
+
 app = Flask(__name__)
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
 class Result(object):
@@ -16,6 +22,18 @@ class Result(object):
 @app.route('/treech-general')
 def get_one_treech_general():
     return treech_general_model.make_sentence(tries=50)
+
+
+
+@app.route('/agro')
+def get_agro():
+    completions = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt="Поругайся в стиле агрессивного школьника, у которого никогда не было секса",
+        max_tokens=300
+    )
+    message = completions.choices[0].text
+    return message
 
 
 @app.route('/treech-general/<int:count>')
