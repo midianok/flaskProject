25c45008aee7b71a26ba0543607c20824a17e300
Ilya Gusev <igusev2@alfabank.ru>
2023-05-02 23:11:41 +0300

    refactor

--- app.py
+++ app.py
@@ -19,42 +19,26 @@ class Result(object):
         self.message = message
 
 
-@app.route('/treech-general')
-def get_one_treech_general():
-    return treech_general_model.make_sentence(tries=50)
-
-
-
-@app.route('/agro')
-def get_agro():
+@app.route('/gpt/<string:prompt>/')
+@app.route('/gpt/<string:prompt>/<int:max_responce_length>')
+def get_agro(prompt, max_responce_length=250):
     completions = openai.Completion.create(
         engine="text-davinci-003",
-        prompt="Поругайся в стиле агрессивного школьника, у которого никогда не было секса",
-        max_tokens=300
+        prompt=prompt,
+        max_tokens=max_responce_length
     )
     message = completions.choices[0].text
     return message
 
 
-@app.route('/treech-general/<int:count>')
-def get_many_treech_general(count=5):
-    strings = []
-    for i in range(1, count + 1):
-        strings.append({"id": i, "message": treech_general_model.make_sentence(tries=50)})
-    return strings
+@app.route('/treech-general')
+def get_one_treech_general():
+    return treech_general_model.make_sentence(tries=50)
 
 
 @app.route('/treech-itan')
 def get_one_treech_itan():
     return treech_itan_model.make_sentence(tries=50)
-
-
-@app.route('/treech-itan/<int:count>')
-def get_many_treech_itan(count=5):
-    strings = []
-    for i in range(1, count + 1):
-        strings.append({"id": i, "message": treech_itan_model.make_sentence(tries=50)})
-    return strings
 
 
 with open("treech_general_v2.txt", encoding="utf8") as f:
